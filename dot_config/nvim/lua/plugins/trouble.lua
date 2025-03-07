e9054ed396a51702e352e0e2fbc61d698f4f1b52
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-07 11:16:58 -0800

    feat: add trouble.nvim plugin to display diagnostics, references

---
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    -- auto_jump = true,
+    -- focus = true,
+    auto_close = true,
+    diagnostics_buffer = {
+      mode = "diagnostics",
+      filter = { buf = 0 },
+    },
+  },
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>tt",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Trouble: toggle diagnostics",
+    },
+  },
+}
