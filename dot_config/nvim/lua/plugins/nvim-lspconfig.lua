3cd02615009051014e8baef066c5e0c33b16b6bc
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-08 09:07:32 -0800

    feat(nvim-lspconfig.lua): enable lua_ls lsp server for lua files

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
-  ft = { "typescript", "typescriptreact" },
+  ft = { "typescript", "typescriptreact", "lua" },
   config = function()
     local lspconfig = require("lspconfig")
     local filetype = vim.bo.filetype
     if filetype == "typescript" or filetype == "typescriptreact" then
       lspconfig.ts_ls.setup({})
+    end
+    if filetype == "lua" then
+      lspconfig.lua_ls.setup({})
     end
   end,
 }
