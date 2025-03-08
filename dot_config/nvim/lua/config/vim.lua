424a101be8ab84fd0787ec67c45513519bc58acf
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-08 08:53:56 -0800

    feat(dot_config/nvim/lua/config/vim.lua): add command to disable syntax highlighting

--- dot_config/nvim/lua/config/vim.lua
+++ dot_config/nvim/lua/config/vim.lua
@@ -95,6 +95,7 @@ vim.keymap.set("n", "<leader>x", ":nohlsearch<cr>", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.cmd("syntax off")
 
 local commands = {
   E = "e",
