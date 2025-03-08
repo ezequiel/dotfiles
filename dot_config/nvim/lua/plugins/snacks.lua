e04ff76f78824fb413f35559ebb25234912d2e00
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-07 20:16:10 -0800

    feat(snacks.lua): add snacks.nvim plugin with file explorer

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,45 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer({ hidden = true })
+      end,
+      desc = "File Explorer",
+    },
+  },
+  ---@type snacks.Config
+  opts = {
+    explorer = { enabled = true },
+    animate = { enabled = false },
+    bigfile = { enabled = false },
+    bufdelete = { enabled = false },
+    dashboard = { enabled = false },
+    debug = { enabled = false },
+    dim = { enabled = false },
+    git = { enabled = false },
+    gitbrowse = { enabled = false },
+    image = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    layout = { enabled = false },
+    lazygit = { enabled = false },
+    notifier = { enabled = false },
+    notify = { enabled = false },
+    picker = { enabled = false },
+    profiler = { enabled = false },
+    quickfile = { enabled = false },
+    rename = { enabled = false },
+    scope = { enabled = false },
+    scratch = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    terminal = { enabled = false },
+    toggle = { enabled = false },
+    util = { enabled = false },
+    win = { enabled = false },
+    words = { enabled = false },
+    zen = { enabled = false },
+  },
+}
