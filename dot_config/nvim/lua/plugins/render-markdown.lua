4d533931fc8c378e038029507f85a68bd66dfb0e
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-07 20:15:38 -0800

    feat(render-markdown): auto-enable conceallevel for markdown files

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -4,6 +4,12 @@ return {
     {
       "nvim-treesitter/nvim-treesitter",
       config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = { "markdown", "Avante" },
+          callback = function()
+            vim.opt.conceallevel = 3
+          end,
+        })
         require("nvim-treesitter.configs").setup({
           highlight = { enable = true },
         })
