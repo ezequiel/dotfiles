b897507e37de74a1226154d471b6366e0e78c3b4
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-08 16:44:51 -0800

    refactor(nvim-lint.lua): run linters on BufWritePost event

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -20,43 +20,47 @@ return {
       typescriptreact = { "eslint_d" },
     }
 
-    vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
-      callback = function()
-        local current_dir = vim.fn.expand("%:h")
+    local function callback()
+      local current_dir = vim.fn.expand("%:h")
 
-        if current_dir == "" then
-          return
-        end
+      if current_dir == "" then
+        return
+      end
 
-        local linters = lint.linters_by_ft[vim.bo.filetype] or {}
-        if not vim.tbl_contains(linters, "eslint_d") then
-          lint.try_lint()
-          return
-        end
+      local linters = lint.linters_by_ft[vim.bo.filetype] or {}
+      if not vim.tbl_contains(linters, "eslint_d") then
+        lint.try_lint()
+        return
+      end
 
-        local function find_eslint_root(dir)
-          while dir do
-            if vim.loop.fs_stat(dir .. "/node_modules/eslint") then
-              return dir
-            end
-            local parent_dir = vim.fs.dirname(dir)
-            if parent_dir == dir then
-              break
-            end
-            dir = parent_dir
+      local function find_eslint_root(dir)
+        while dir do
+          if vim.loop.fs_stat(dir .. "/node_modules/eslint") then
+            return dir
           end
-          return nil
+          local parent_dir = vim.fs.dirname(dir)
+          if parent_dir == dir then
+            break
+          end
+          dir = parent_dir
         end
+        return nil
+      end
 
-        local eslint_root = find_eslint_root(current_dir)
-        if eslint_root then
-          lint.try_lint("eslint_d", { cwd = eslint_root })
-        end
+      local eslint_root = find_eslint_root(current_dir)
+      if eslint_root then
+        lint.try_lint("eslint_d", { cwd = eslint_root })
+      end
 
-        lint.try_lint(vim.tbl_filter(function(linter)
-          return linter ~= "eslint_d"
-        end, linters))
-      end,
+      lint.try_lint(vim.tbl_filter(function(linter)
+        return linter ~= "eslint_d"
+      end, linters))
+    end
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = callback,
     })
+
+    callback()
   end,
 }
