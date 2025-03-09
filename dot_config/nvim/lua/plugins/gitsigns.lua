572fc9b732ac318b50a2d6b371b9c41bcd89b4dd
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-08 22:33:16 -0800

    feat(gitsigns.lua): configure gitsigns plugin with custom keymaps for hunk navigation and staging

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,43 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+
+          map("n", "<leader>hn", function()
+            if vim.wo.diff then
+              vim.cmd.normal({ "]c", bang = true })
+            else
+              gs.nav_hunk("next")
+            end
+          end, "Next Hunk")
+
+          map("n", "<leader>hp", function()
+            if vim.wo.diff then
+              vim.cmd.normal({ "[c", bang = true })
+            else
+              gs.nav_hunk("prev")
+            end
+          end, "Prev Hunk")
+
+          map("n", "<leader>hl", function()
+            gs.nav_hunk("last")
+          end, "Last Hunk")
+
+          map("n", "<leader>hf", function()
+            gs.nav_hunk("first")
+          end, "First Hunk")
+
+          map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+          map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+          map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        end,
+      })
     end,
   },
 }
