a295476f76b5097379b274a7153c2e5f025e932d
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-10 15:33:12 -0700

    feat: add keymaps for navigating quickfix list

--- dot_config/nvim/lua/config/vim.lua
+++ dot_config/nvim/lua/config/vim.lua
@@ -98,6 +98,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "gf", ":GFI<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 vim.cmd("syntax off")
 
