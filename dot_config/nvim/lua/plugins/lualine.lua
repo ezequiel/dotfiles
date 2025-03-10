9d973393e8b595d783fda186b572ba9dceafe6a8
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-09 23:01:53 -0700

    feat: set up lualine status bar with tokyonight theme

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight",
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          "branch",
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          },
+        },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
+}
