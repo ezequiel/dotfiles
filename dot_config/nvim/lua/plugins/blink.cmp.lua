c35b46ff80ec311a85cd86c1b71adeda32a45bdf
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-10 18:34:10 -0700

    feat(blink.cmp.lua): configure blink completion plugin with enhanced UI and features

--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -2,12 +2,22 @@ return {
   "saghen/blink.cmp",
   event = "InsertEnter",
   version = "*",
+  -- opts_extend = { "sources.default" },
   opts = {
     keymap = { preset = "enter" },
+    -- opts_extend = { "sources.default" },
     completion = {
       documentation = { auto_show = true, treesitter_highlighting = true },
       ghost_text = { enabled = true },
       accept = { auto_brackets = { enabled = true } },
+      menu = {
+        draw = {
+          columns = {
+            -- kind, source_id, source_name, kind_icon, label, label_description
+            { "kind_icon", "label", "label_description", gap = 1 },
+          },
+        },
+      },
     },
     signature = { enabled = true, window = { show_documentation = true } },
     appearance = {
