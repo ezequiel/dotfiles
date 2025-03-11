bf82155bc1009fc052cfa2be39502b05c3523070
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-10 21:55:51 -0700

    feat(grug-far.lua): add <leader>gb mapping to prefill current file path

--- dot_config/nvim/lua/plugins/grug-far.lua
+++ dot_config/nvim/lua/plugins/grug-far.lua
@@ -8,6 +8,13 @@ return {
       end,
       mode = { "n", "v", "x", "o" },
     },
+    {
+      "<leader>gb",
+      function()
+        require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+      end,
+      mode = { "n", "v", "x", "o" },
+    },
   },
   config = function(_, opts)
     require("grug-far").setup(opts)
