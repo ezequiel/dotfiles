f91f8259332bf3d6624fa724378a41e6410ec35e
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-11 20:36:57 -0700

    feat(blink.cmp.lua): improve <CR> keymap to handle snippets

--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -5,7 +5,17 @@ return {
   opts = {
     keymap = {
       preset = "super-tab",
-      ["<cr>"] = { "select_and_accept" },
+      ["<CR>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
     },
     completion = {
       documentation = {
