2bb9a37729ab063603ee3c3b411e714979d9a484
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-11 19:56:52 -0700

    fix(yanky.lua): change modes for yanky keys from "x" to "v"

--- dot_config/nvim/lua/plugins/yanky.lua
+++ dot_config/nvim/lua/plugins/yanky.lua
@@ -7,8 +7,8 @@ return {
     ring = { storage = "sqlite" },
   },
   keys = {
-    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+    { "y", "<Plug>(YankyYank)", mode = { "n", "v" }, desc = "Yank text" },
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "v" }, desc = "Put yanked text after cursor" },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "v" }, desc = "Put yanked text before cursor" },
   },
 }
