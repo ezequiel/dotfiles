fd984c96e45fd5200ac4745016b6dd4b4c2f6446
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-12 20:36:57 -0700

    feat: remove nvim-autopairs plugin, add window navigation keymaps to visual mode

--- dot_config/nvim/lua/config/vim.lua
+++ dot_config/nvim/lua/config/vim.lua
@@ -82,8 +82,8 @@ vim.diagnostic.config({
   },
 })
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set({ "n", "v" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n", "v" }, "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "j", "gj")
