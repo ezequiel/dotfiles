69229c4e6167c1286ef71ad0c4753b18a06ca9da
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-13 22:07:48 -0700

    feat: add "VeryLazy" event to Comment and nvim-treesitter plugins

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,44 +1,43 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      -- rainbow = {
-      --   enable = true,
-      --   extended_mode = true,
-      --   max_file_lines = nil,
-      -- },
-      indent = { enable = true },
-      autotag = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-      },
-      ensure_installed = {
-        "javascript",
-        "typescript",
-        "tsx",
-        "vimdoc",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "json",
-        "jsonc",
-        "regex",
-        "bash",
-        "go",
-        "gomod",
-        "vim",
-      },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = "VeryLazy",
+  opts = {
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    -- rainbow = {
+    --   enable = true,
+    --   extended_mode = true,
+    --   max_file_lines = nil,
+    -- },
+    indent = { enable = true },
+    autotag = {
+      enable = true,
+    },
+    matchup = {
+      enable = true,
+    },
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "tsx",
+      "vimdoc",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "json",
+      "jsonc",
+      "regex",
+      "bash",
+      "go",
+      "gomod",
+      "vim",
+    },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
