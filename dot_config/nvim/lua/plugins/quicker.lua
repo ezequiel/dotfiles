5db044129e7a4772cb975b7a9a91ed4939e3d181
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-14 01:41:22 -0700

    feat: add quicker.nvim plugin for improved quickfix navigation

---
+++ dot_config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/quicker.nvim",
+  ft = "qf",
+  opts = {
+    borders = {
+      vert = " | ",
+      strong_header = "-",
+      strong_cross = "-",
+      strong_end = " | ",
+      soft_header = "╌",
+      soft_cross = " | ",
+      soft_end = " | ",
+    },
+    trim_leading_whitespace = "all",
+  },
+}
