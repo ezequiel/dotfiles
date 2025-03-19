78158317dc32817e1e7fe3081f957109e8d0309a
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-19 13:03:17 -0700

    feat(oil.lua): configure oil.nvim plugin with enhanced features
    The configuration introduces several new features and improvements to the oil.nvim plugin:
    
    - Sets `lazy = false` to load the plugin immediately.
    - Enables `watch_for_changes` to automatically update the file explorer.
    - Configures `view_options` to show hidden files.
    - Disables default keymaps and introduces custom keymaps for navigation and actions.
    - Adds `skip_confirm_for_simple_edits` to skip confirmation prompts for simple file edits.
    - Sets `constrain_cursor` to "name" to constrain the cursor to the file name.
    - Configures `lsp_file_methods` to autosave changes.
    - Sets `preview_split` to "right" to open previews in a right-hand split.
    - Implements a custom keybinding `<leader>o` to toggle the oil float and open a preview of the selected file.

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,20 +1,10 @@
 return {
   "stevearc/oil.nvim",
+  lazy = false,
   opts = {
     watch_for_changes = true,
     view_options = {
       show_hidden = true,
-    },
-    git = {
-      add = function()
-        return true
-      end,
-      mv = function()
-        return true
-      end,
-      rm = function()
-        return true
-      end,
     },
     keymaps = {
       ["?"] = { "actions.show_help", mode = "n" },
@@ -25,13 +15,28 @@ return {
       ["<BS>"] = { "actions.parent", mode = "n" },
     },
     use_default_keymaps = false,
+    skip_confirm_for_simple_edits = true,
+    constrain_cursor = "name",
+    lsp_file_methods = {
+      autosave_changes = true,
+    },
+    preview_split = "right",
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     {
       "<leader>o",
       function()
-        require("oil").open()
+        local oil = require("oil")
+        oil.toggle_float()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "OilEnter",
+          callback = vim.schedule_wrap(function(args)
+            if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+              oil.open_preview()
+            end
+          end),
+        })
       end,
     },
   },
