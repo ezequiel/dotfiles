af1fe3bf05b2c50c6e1bca701929dc095de57116
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-19 13:06:03 -0700

    feat(nvim): add autocmd to run TSC on AutoSaveWritePost event

--- dot_config/nvim/lua/plugins/tsc.lua
+++ dot_config/nvim/lua/plugins/tsc.lua
@@ -14,17 +14,22 @@ return {
       auto_open_qflist = false,
       auto_close_qflist = false,
       enable_progress_notifications = false,
-
       -- use_trouble_qflist = true,
       -- bin_path = require("lspconfig").util.root_pattern("package.json", ".git"),
       -- bin_path = require("tsc.utils").find_tsc_bin(),
       -- pretty_errors = true,
       flags = {
-        watch = true,
+        -- watch = true,
         noEmit = true,
       },
     })
 
     vim.cmd("TSC")
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "AutoSaveWritePost",
+      callback = function()
+        vim.cmd("TSC")
+      end,
+    })
   end,
 }
