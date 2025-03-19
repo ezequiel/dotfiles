ee4af846aef672fc3a889a89e326ddeb0e706223
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-19 13:04:10 -0700

    feat(noice.lua): disable signature help and remove LSP overrides

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -2,18 +2,15 @@ return {
   "folke/noice.nvim",
   opts = {
     lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
       hover = {
         silent = true,
+      },
+      signature = {
+        enabled = false,
       },
     },
     presets = {
       lsp_doc_border = true,
-      long_message_to_split = true,
     },
   },
   dependencies = {
