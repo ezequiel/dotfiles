b792ead179fad5140e54d8edfeb219b46eb2ec60
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-20 20:49:40 -0700

    feat(conform.lua): add taplo formatter for toml files

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,47 +1,48 @@
 return {
-  "stevearc/conform.nvim",
-  event = { { event = "User", pattern = "AutoSaveWritePost" } },
-  opts = {
-    formatters_by_ft = {
-      ["*"] = { "trim_newlines", "trim_whitespace" },
-      css = { "prettierd" },
-      go = { "goimports", "gofmt" },
-      html = { "prettierd" },
-      htmlangular = { "prettierd" },
-      javascript = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      json = { "prettierd" },
-      jsonc = { "prettierd" },
-      lua = { "stylua" },
-      markdown = { "prettierd" },
-      scss = { "prettierd" },
-      typescript = { "prettierd" },
-      typescriptreact = { "prettierd" },
-      yaml = { "prettierd" },
-    },
-    formatters = {
-      prettierd = {
-        env = {
-          PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
-        },
-      },
-    },
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-    log_level = vim.log.levels.ERROR,
-    notify_on_error = false,
-    notify_no_formatters = true,
-  },
-  config = function(_, opts)
-    local conform = require("conform")
-    conform.setup(opts)
-    conform.format({ bufnr = vim.api.nvim_get_current_buf() })
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "AutoSaveWritePost",
-      callback = function(stuff)
-        conform.format({ bufnr = stuff.data.saved_buffer })
-      end,
-    })
-  end,
+	"stevearc/conform.nvim",
+	event = { { event = "User", pattern = "AutoSaveWritePost" } },
+	opts = {
+		formatters_by_ft = {
+			["*"] = { "trim_newlines", "trim_whitespace" },
+			css = { "prettierd" },
+			go = { "goimports", "gofmt" },
+			html = { "prettierd" },
+			htmlangular = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			json = { "prettierd" },
+			jsonc = { "prettierd" },
+			lua = { "stylua" },
+			markdown = { "prettierd" },
+			scss = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			yaml = { "prettierd" },
+			toml = { "taplo" },
+		},
+		formatters = {
+			prettierd = {
+				env = {
+					PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
+				},
+			},
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		log_level = vim.log.levels.ERROR,
+		notify_on_error = false,
+		notify_no_formatters = true,
+	},
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+		conform.format({ bufnr = vim.api.nvim_get_current_buf() })
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "AutoSaveWritePost",
+			callback = function(stuff)
+				conform.format({ bufnr = stuff.data.saved_buffer })
+			end,
+		})
+	end,
 }
