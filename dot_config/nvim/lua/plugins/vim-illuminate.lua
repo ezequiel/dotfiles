ffef16e4ecbfb2f6a5ff502de15edaeddda3e67c
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-22 15:27:54 -0700

    feat(vim-illuminate): disable illuminate when multicursor is active

--- dot_config/nvim/lua/plugins/vim-illuminate.lua
+++ dot_config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,15 +1,19 @@
 return {
-	"RRethy/vim-illuminate",
-	event = "VeryLazy",
-	config = function()
-		require("illuminate").configure({
-			providers = {
-				"lsp",
-				"treesitter",
-			},
-			delay = 350,
-			disable_keymaps = true,
-			-- filetypes_denylist = {},
-		})
-	end,
+  "RRethy/vim-illuminate",
+  event = "VeryLazy",
+  config = function()
+    require("illuminate").configure({
+      providers = {
+        "lsp",
+        "treesitter",
+      },
+      delay = 350,
+      disable_keymaps = true,
+      should_enable = function()
+        local multicursor = require("multicursor-nvim")
+        return not multicursor.hasCursors()
+      end,
+      -- filetypes_denylist = {},
+    })
+  end,
 }
