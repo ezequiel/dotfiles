ef5c114b4e9629bed531f65fdd5bc05238c396e1
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-23 21:54:40 -0700

    feat(yazi.lua): add grep and replace keybinds and integrations

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -23,11 +23,15 @@ return {
       open_file_in_horizontal_split = "-",
       send_to_quickfix_list = "<c-q>",
       open_file_in_tab = false,
-      grep_in_directory = false,
-      replace_in_directory = false,
+      grep_in_directory = "<c-s>",
+      replace_in_directory = "<c-r>",
       cycle_open_buffers = false,
       copy_relative_path_to_selected_files = false,
       change_working_directory = false,
+    },
+    integrations = {
+      grep_in_directory = "snacks.picker",
+      grep_in_selected_files = "snacks.picker",
     },
   },
   init = function()
