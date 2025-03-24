3ddc9e5420ad91563ccef005b77b0ee87a0ce5b6
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-23 17:39:40 -0700

    fix(nvim): set guicursor to improve cursor visibility in Neovim

--- dot_config/nvim/lua/config/opt.lua
+++ dot_config/nvim/lua/config/opt.lua
@@ -52,7 +52,8 @@ vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.errorbells = false
 vim.opt.visualbell = false
-vim.opt.guicursor = ""
+vim.opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 5
 vim.opt.shortmess:append("I")
