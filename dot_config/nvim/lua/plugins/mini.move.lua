d0efdd6578e66f061e7161bd8e431b4c518a72c7
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-29 12:34:19 -0700

    feat: add mini.move plugin to move lines and selections

---
+++ dot_config/nvim/lua/plugins/mini.move.lua
@@ -0,0 +1,25 @@
+return {
+  "echasnovski/mini.move",
+  version = "*",
+  keys = {
+    { "<S-j>", "<cmd>lua MiniMove.move_line('down')<cr>", mode = { "n" } },
+    { "<S-k>", "<cmd>lua MiniMove.move_line('up')<cr>", mode = { "n" } },
+    { "<S-j>", "<cmd>lua MiniMove.move_selection('down')<cr>", mode = { "x" } },
+    { "<S-k>", "<cmd>lua MiniMove.move_selection('up')<cr>", mode = { "x" } },
+  },
+  opts = {
+    mappings = {
+      left = "",
+      right = "",
+      down = "",
+      up = "",
+      line_left = "",
+      line_right = "",
+      line_down = "",
+      line_up = "",
+    },
+  },
+  config = function(_, opts)
+    require("mini.move").setup(opts)
+  end,
+}
