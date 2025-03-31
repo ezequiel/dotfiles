f6c343795fd6fe31479d173f3663aa005fe3ae94
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-30 20:48:23 -0700

    feat(keymap.lua): add keymaps for navigating to the next/previous separator line

--- dot_config/nvim/lua/config/keymap.lua
+++ dot_config/nvim/lua/config/keymap.lua
@@ -51,3 +51,11 @@ vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next match" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous match" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.setqflist, { desc = "Open diagnostics in quickfix list" })
 vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+local MID = "^=======$"
+vim.keymap.set("n", "[x", function()
+  vim.cmd("?" .. MID)
+end)
+vim.keymap.set("n", "]x", function()
+  vim.cmd("/" .. MID)
+end)
