564d1460cbf6a5b9ed151391ce581fe1dc04e57c
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-03-31 20:50:47 -0700

    feat(render-markdown): add codecompanion filetype to render markdown plugin

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,11 +1,12 @@
+local file_types = { "norg", "markdown", "Avante", "qf", "codecompanion" }
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
-    file_types = { "norg", "markdown", "Avante", "qf" },
+    file_types = file_types,
   },
   init = function()
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "markdown", "Avante", "norg" },
+      pattern = file_types,
       callback = function()
         vim.opt.conceallevel = 3
       end,
