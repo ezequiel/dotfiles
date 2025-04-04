3b49139fb6ab0ba481fb4ef0f791e0547b6faa38
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-04 02:25:07 -0700

    refactor(vim-illuminate): remove multicursor check from illuminate config

--- dot_config/nvim/lua/plugins/vim-illuminate.lua
+++ dot_config/nvim/lua/plugins/vim-illuminate.lua
@@ -10,11 +10,6 @@ return {
       under_cursor = false,
       delay = 350,
       disable_keymaps = true,
-      should_enable = function()
-        local multicursor = require("multicursor-nvim")
-        return not multicursor.hasCursors()
-      end,
-      -- filetypes_denylist = {},
     })
   end,
 }
