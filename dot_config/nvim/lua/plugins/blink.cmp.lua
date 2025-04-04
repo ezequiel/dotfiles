4c332df847f0ef5e94dcc9a693fde59c4bfc92cc
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-03 20:10:24 -0700

    fix(nvim): set `auto_insert` to true for cmp selection list

--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -64,7 +64,7 @@ return {
       list = {
         selection = {
           preselect = false,
-          auto_insert = false,
+          auto_insert = true,
         },
       },
       ghost_text = { enabled = false },
