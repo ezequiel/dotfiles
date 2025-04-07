d5acfad36afe66e48d5c87645b02351f4480a0db
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-07 01:28:58 -0700

    feat(snacks.lua): add snacks plugin configuration
    This commit introduces the configuration for the snacks.nvim plugin,
    enhancing file finding and buffer management within Neovim. It includes
    key mappings for various snacks picker functions like git status, files,
    buffers, grep, and more. Also adds keybind to close buffer.

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,5 +1,5 @@
 local function get_cwd()
-  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel 2>/dev/null")[1]
+  local git_root = vim.fn.systemlist('git rev-parse --show-toplevel 2>/dev/null')[1]
   if vim.v.shell_error ~= 0 then
     return vim.fn.getcwd()
   end
@@ -7,59 +7,59 @@ local function get_cwd()
 end
 
 return {
-  "folke/snacks.nvim",
+  'folke/snacks.nvim',
   lazy = false,
   priority = 1000,
   dependencies = {
-    "tpope/vim-sleuth",
+    'tpope/vim-sleuth',
   },
   keys = {
     {
-      "<leader>re",
+      '<leader>re',
       function()
         Snacks.picker.resume()
       end,
-      desc = "Resume",
+      desc = 'Resume',
     },
     {
-      "<leader>gs",
+      '<leader>gs',
       function()
         Snacks.picker.git_status()
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>ff",
+      '<leader>ff',
       function()
         Snacks.picker.files({ cwd = get_cwd() })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>bb",
+      '<leader>bb',
       function()
         Snacks.picker.buffers()
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>rg",
+      '<leader>rg',
       function()
         Snacks.picker.grep({ cwd = get_cwd() })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>rgb",
+      '<leader>rgb',
       function()
         Snacks.picker.grep({
-          dirs = { vim.fn.expand("%") },
+          dirs = { vim.fn.expand('%') },
         })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>rgw",
+      '<leader>rgw',
       function()
         Snacks.picker.grep_word({
           cwd = get_cwd(),
@@ -68,22 +68,22 @@ return {
           },
         })
       end,
-      mode = { "n" },
+      mode = { 'n' },
     },
     {
-      "<leader>rgwb",
+      '<leader>rgwb',
       function()
         Snacks.picker.grep_word({
-          dirs = { vim.fn.expand("%") },
+          dirs = { vim.fn.expand('%') },
           matcher = {
             fuzzy = false,
           },
         })
       end,
-      mode = { "n" },
+      mode = { 'n' },
     },
     {
-      "<leader>rgv",
+      '<leader>rgv',
       function()
         Snacks.picker.grep_word({
           cwd = get_cwd(),
@@ -92,22 +92,22 @@ return {
           },
         })
       end,
-      mode = { "x" },
+      mode = { 'x' },
     },
     {
-      "<leader>rgvb",
+      '<leader>rgvb',
       function()
         Snacks.picker.grep_word({
-          dirs = { vim.fn.expand("%") },
+          dirs = { vim.fn.expand('%') },
           matcher = {
             fuzzy = false,
           },
         })
       end,
-      mode = { "x" },
+      mode = { 'x' },
     },
     {
-      "<leader>rgbl",
+      '<leader>rgbl',
       function()
         Snacks.picker.lines({
           matcher = {
@@ -115,35 +115,36 @@ return {
           },
         })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<C-x>",
+      '<C-x>',
       function()
         Snacks.bufdelete()
+        vim.cmd('close')
       end,
     },
     {
-      "gd",
+      'gd',
       function()
         Snacks.picker.lsp_definitions()
       end,
     },
     {
-      "gr",
+      'gr',
       function()
         Snacks.picker.lsp_references()
       end,
       nowait = true,
     },
     {
-      "gi",
+      'gi',
       function()
         Snacks.picker.lsp_implementations()
       end,
     },
     {
-      "gt",
+      'gt',
       function()
         Snacks.picker.lsp_type_definitions()
       end,
@@ -175,59 +176,59 @@ return {
         },
         input = {
           keys = {
-            ["/"] = false,
-            ["<C-Down>"] = false,
-            ["<C-Up>"] = false,
-            ["<C-c>"] = { "close", mode = { "i", "x", "n" } },
-            ["<C-w>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
-            ["<a-BS>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
-            ["<CR>"] = { "confirm", mode = { "n", "i" } },
-            ["<Down>"] = { "list_down", mode = { "i", "n" } },
-            ["<Esc>"] = "cancel",
-            ["<S-CR>"] = false,
-            ["<S-Tab>"] = false,
-            ["<Tab>"] = false,
-            ["<Up>"] = { "list_up", mode = { "i", "n" } },
-            ["<a-d>"] = false,
-            ["<a-f>"] = false,
-            ["<a-h>"] = false,
-            ["<a-i>"] = false,
-            ["<a-m>"] = false,
-            ["<a-p>"] = false,
-            ["<a-w>"] = false,
-            ["<c-a>"] = false,
-            ["<c-b>"] = false,
-            ["<c-d>"] = { "list_scroll_down", mode = { "i", "n" } },
-            ["<c-f>"] = false,
-            ["<c-g>"] = { "toggle_live", mode = { "i", "n" } },
-            ["<c-j>"] = false,
-            ["<c-k>"] = false,
-            ["<c-q>"] = { "qflist", mode = { "i", "n" } },
-            ["<c-s>"] = false,
-            ["<c-t>"] = false,
-            ["<c-u>"] = { "list_scroll_up", mode = { "i", "n" } },
-            ["<c-v>"] = false,
-            ["<c-r>#"] = false,
-            ["<c-r>%"] = false,
-            ["<c-r><c-a>"] = false,
-            ["<c-r><c-f>"] = false,
-            ["<c-r><c-l>"] = false,
-            ["<c-r><c-p>"] = false,
-            ["<c-r><c-w>"] = false,
-            ["<c-w>H"] = false,
-            ["<c-w>J"] = false,
-            ["<c-w>K"] = false,
-            ["<c-w>L"] = false,
-            ["?"] = "toggle_help_input",
-            ["G"] = "list_bottom",
-            ["gg"] = "list_top",
-            ["j"] = "list_down",
-            ["k"] = "list_up",
-            ["<c-n>"] = { "list_down", mode = { "i", "n" } },
-            ["<c-p>"] = { "list_up", mode = { "i", "n" } },
-            ["q"] = "close",
-            ["<S-Up>"] = { "select_and_prev", mode = { "i", "n" } },
-            ["<S-Down>"] = { "select_and_next", mode = { "i", "n" } },
+            ['/'] = false,
+            ['<C-Down>'] = false,
+            ['<C-Up>'] = false,
+            ['<C-c>'] = { 'close', mode = { 'i', 'x', 'n' } },
+            ['<C-w>'] = { '<c-s-w>', mode = { 'i' }, expr = true, desc = 'delete word' },
+            ['<a-BS>'] = { '<c-s-w>', mode = { 'i' }, expr = true, desc = 'delete word' },
+            ['<CR>'] = { 'confirm', mode = { 'n', 'i' } },
+            ['<Down>'] = { 'list_down', mode = { 'i', 'n' } },
+            ['<Esc>'] = 'cancel',
+            ['<S-CR>'] = false,
+            ['<S-Tab>'] = false,
+            ['<Tab>'] = false,
+            ['<Up>'] = { 'list_up', mode = { 'i', 'n' } },
+            ['<a-d>'] = false,
+            ['<a-f>'] = false,
+            ['<a-h>'] = false,
+            ['<a-i>'] = false,
+            ['<a-m>'] = false,
+            ['<a-p>'] = false,
+            ['<a-w>'] = false,
+            ['<c-a>'] = false,
+            ['<c-b>'] = false,
+            ['<c-d>'] = { 'list_scroll_down', mode = { 'i', 'n' } },
+            ['<c-f>'] = false,
+            ['<c-g>'] = { 'toggle_live', mode = { 'i', 'n' } },
+            ['<c-j>'] = false,
+            ['<c-k>'] = false,
+            ['<c-q>'] = { 'qflist', mode = { 'i', 'n' } },
+            ['<c-s>'] = false,
+            ['<c-t>'] = false,
+            ['<c-u>'] = { 'list_scroll_up', mode = { 'i', 'n' } },
+            ['<c-v>'] = false,
+            ['<c-r>#'] = false,
+            ['<c-r>%'] = false,
+            ['<c-r><c-a>'] = false,
+            ['<c-r><c-f>'] = false,
+            ['<c-r><c-l>'] = false,
+            ['<c-r><c-p>'] = false,
+            ['<c-r><c-w>'] = false,
+            ['<c-w>H'] = false,
+            ['<c-w>J'] = false,
+            ['<c-w>K'] = false,
+            ['<c-w>L'] = false,
+            ['?'] = 'toggle_help_input',
+            ['G'] = 'list_bottom',
+            ['gg'] = 'list_top',
+            ['j'] = 'list_down',
+            ['k'] = 'list_up',
+            ['<c-n>'] = { 'list_down', mode = { 'i', 'n' } },
+            ['<c-p>'] = { 'list_up', mode = { 'i', 'n' } },
+            ['q'] = 'close',
+            ['<S-Up>'] = { 'select_and_prev', mode = { 'i', 'n' } },
+            ['<S-Down>'] = { 'select_and_next', mode = { 'i', 'n' } },
           },
         },
       },
