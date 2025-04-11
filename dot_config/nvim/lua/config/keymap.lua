35df91e4fafa265816105d4115cb4a64d4fd296d
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-11 15:54:10 -0700

    feat(keymap.lua): enhance quickfix navigation and search functionality
    This commit introduces several enhancements to the key mappings:
    
    - Improves quickfix navigation with <C-n> and <C-p> to handle same file
      and line scenarios.
    - Adds search functionality with * to search for the current word.

--- dot_config/nvim/lua/config/keymap.lua
+++ dot_config/nvim/lua/config/keymap.lua
@@ -1,72 +1,114 @@
-vim.keymap.set({ "n", "x" }, "<C-h>", "<C-w>h")
-vim.keymap.set({ "n", "x" }, "<C-l>", "<C-w>l")
-vim.keymap.set({ "n", "x" }, "<C-j>", "<C-w>j")
-vim.keymap.set({ "n", "x" }, "<C-k>", "<C-w>k")
-vim.keymap.set({ "n", "x" }, "<C-n>", function()
-  local qf = vim.fn.getqflist({ idx = 0, size = 0 })
+vim.keymap.set({ 'n', 'x' }, '<C-h>', '<C-w>h')
+vim.keymap.set({ 'n', 'x' }, '<C-l>', '<C-w>l')
+vim.keymap.set({ 'n', 'x' }, '<C-j>', '<C-w>j')
+vim.keymap.set({ 'n', 'x' }, '<C-k>', '<C-w>k')
+vim.keymap.set({ 'n', 'x' }, '<C-n>', function()
+  local qf = vim.fn.getqflist({ idx = 0, size = 0, items = 1 })
   if qf.idx >= qf.size then
     return
   end
-  vim.cmd("cnext")
+
+  local item = qf.items[qf.idx]
+  if not item then
+    return
+  end
+
+  local current_path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':p')
+  local qf_path = vim.fn.fnamemodify(vim.fn.bufname(item.bufnr), ':p')
+  local current_line = vim.api.nvim_win_get_cursor(0)[1]
+
+  local same_file = current_path == qf_path
+  local same_line = current_line == item.lnum
+
+  if same_file and same_line then
+    vim.cmd('cnext')
+  else
+    vim.api.nvim_set_current_buf(item.bufnr)
+    vim.api.nvim_win_set_cursor(0, { item.lnum, item.col > 0 and item.col - 1 or 0 })
+  end
 end, { silent = true })
-vim.keymap.set({ "n", "x" }, "<C-p>", function()
+vim.keymap.set({ 'n', 'x' }, '<C-p>', function()
   local qf = vim.fn.getqflist({ idx = 0, size = 0 })
   if qf.idx <= 1 then
     return
   end
-  vim.cmd("cprev")
+  vim.cmd('cprev')
 end, { silent = true })
-vim.keymap.set(
-  { "n", "i", "x" },
-  "<Esc>",
-  "<Esc><cmd>doautocmd FocusLost<CR><cmd>nohlsearch<CR><cmd>fclose!<cr>",
-  { noremap = true }
-)
-vim.keymap.set(
-  { "n", "i", "x" },
-  "<C-c>",
-  "<C-c><cmd>doautocmd FocusLost<CR><cmd>nohlsearch<CR><cmd>fclose!<cr>",
-  { noremap = true }
-)
-vim.keymap.set("n", "<s-tab>", "<cmd>bp<CR>")
-vim.keymap.set("n", "<tab>", "<cmd>bn<CR>")
-vim.keymap.set("n", "J", "<nop>", { silent = true })
-vim.keymap.set("n", "Q", "<nop>", { silent = true })
-vim.keymap.set("n", "q", "<nop>", { silent = true })
-vim.keymap.set("n", "vv", "V", { noremap = true, silent = true })
-vim.keymap.set("x", "/", "<Esc>/\\%V")
-vim.keymap.set("x", "r", [[:s/\%V]])
-vim.keymap.set({ "n", "x" }, "*", function()
-  vim.fn.setreg("/", "")
-  local search_text
-  if vim.fn.mode() == "v" or vim.fn.mode() == "x" then
-    vim.cmd('normal! "zy')
-    search_text = vim.fn.getreg("z")
+vim.keymap.set({ 'n', 'x' }, '<C-p>', function()
+  local qf = vim.fn.getqflist({ idx = 0, size = 0, items = 1 })
+  local item = qf.items[qf.idx]
+
+  if not item then
+    return
+  end
+
+  local current_path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':p')
+  local qf_path = vim.fn.fnamemodify(vim.fn.bufname(item.bufnr), ':p')
+  local current_line = vim.api.nvim_win_get_cursor(0)[1]
+
+  local same_file = current_path == qf_path
+  local same_line = current_line == item.lnum
+
+  if same_file and same_line then
+    if qf.idx > 1 then
+      vim.cmd('cprev')
+    end
   else
-    search_text = vim.fn.expand("<cword>")
+    vim.api.nvim_set_current_buf(item.bufnr)
+    vim.api.nvim_win_set_cursor(0, { item.lnum, item.col > 0 and item.col - 1 or 0 })
   end
-  search_text = vim.fn.escape(search_text, "\\^$.*[]")
+end, { silent = true })
+vim.keymap.set(
+  { 'n', 'i', 'x' },
+  '<Esc>',
+  '<Esc><cmd>doautocmd FocusLost<CR><cmd>nohlsearch<CR><cmd>fclose!<cr>',
+  { noremap = true }
+)
+vim.keymap.set(
+  { 'n', 'i', 'x' },
+  '<C-c>',
+  '<C-c><cmd>doautocmd FocusLost<CR><cmd>nohlsearch<CR><cmd>fclose!<cr>',
+  { noremap = true }
+)
+vim.keymap.set('n', '<s-tab>', '<cmd>bp<CR>')
+vim.keymap.set('n', '<tab>', '<cmd>bn<CR>')
+vim.keymap.set('n', 'J', '<nop>', { silent = true })
+vim.keymap.set('n', 'Q', '<nop>', { silent = true })
+vim.keymap.set('n', 'q', '<nop>', { silent = true })
+vim.keymap.set('n', 'vv', 'V', { noremap = true, silent = true })
+vim.keymap.set('x', '/', '<Esc>/\\%V')
+vim.keymap.set('x', 'r', [[:s/\%V]])
+vim.keymap.set({ 'n', 'x' }, '*', function()
+  vim.fn.setreg('/', '')
+  local search_text
+  if vim.fn.mode() == 'v' or vim.fn.mode() == 'x' then
+    vim.cmd('normal! "zy')
+    search_text = vim.fn.getreg('z')
+  else
+    search_text = vim.fn.expand('<cword>')
+  end
+  search_text = vim.fn.escape(search_text, '\\^$.*[]')
 
-  if search_text:match("^%w+$") then
-    search_text = "\\<" .. search_text .. "\\>"
+  if search_text:match('^%w+$') then
+    search_text = '\\<' .. search_text .. '\\>'
   end
 
-  vim.fn.setreg("/", search_text)
+  vim.fn.setreg('/', search_text)
   vim.opt.hlsearch = true
-  vim.cmd("normal! nN")
+  vim.cmd('normal! nN')
 end, { silent = true })
 
-vim.keymap.set("n", "<C-W>|", "<C-W>v", { noremap = true })
-vim.keymap.set("n", "<C-W>-", "<C-W>s", { noremap = true })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next match" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous match" })
-vim.keymap.set("n", "<leader>ds", vim.diagnostic.setqflist, { desc = "Open diagnostics in quickfix list" })
-vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set('n', '<C-W>|', '<C-W>v', { noremap = true })
+vim.keymap.set('n', '<C-W>-', '<C-W>s', { noremap = true })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Move to next match' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Move to previous match' })
+vim.keymap.set('n', '<leader>ds', vim.diagnostic.setqflist, { desc = 'Open diagnostics in quickfix list' })
+vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { noremap = true, silent = true })
 
-local MID = "^=======$"
-vim.keymap.set("n", "[x", function()
-  vim.cmd("?" .. MID)
+local MID = '^=======$'
+vim.keymap.set('n', '[x', function()
+  vim.cmd('?' .. MID)
 end)
-vim.keymap.set("n", "]x", function()
-  vim.cmd("/" .. MID)
+vim.keymap.set('n', ']x', function()
+  vim.cmd('/' .. MID)
 end)
