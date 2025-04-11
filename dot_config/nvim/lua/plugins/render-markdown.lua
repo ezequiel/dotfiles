fc6e9b8e07a6cace4cfe9951ac7d6784e17888be
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-11 15:53:05 -0700

    fix(render-markdown): use single quotes for string literals in lua config

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,12 +1,12 @@
-local file_types = { "norg", "markdown", "qf", "codecompanion" }
+local file_types = { 'norg', 'markdown', 'qf', 'codecompanion' }
 return {
   ft = file_types,
-  "MeanderingProgrammer/render-markdown.nvim",
+  'MeanderingProgrammer/render-markdown.nvim',
   opts = {
     file_types = file_types,
   },
   init = function()
-    vim.api.nvim_create_autocmd("FileType", {
+    vim.api.nvim_create_autocmd('FileType', {
       pattern = file_types,
       callback = function()
         vim.opt.conceallevel = 3
