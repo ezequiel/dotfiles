0bed00f39bc61e6593fe8f8a72e4af92a2686cbd
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-11 15:52:57 -0700

    refactor(cmd.lua): use single quotes for strings and update autocmds feat(cmd.lua): add autocmd to kill prettierd on exit feat(cmd.lua): link Folded highlight group to Normal for color consistency

--- dot_config/nvim/lua/config/cmd.lua
+++ dot_config/nvim/lua/config/cmd.lua
@@ -1,56 +1,56 @@
-vim.cmd("syntax off")
+vim.cmd('syntax off')
 
-vim.api.nvim_create_autocmd("VimResized", {
-  pattern = "*",
-  command = "wincmd =",
+vim.api.nvim_create_autocmd('VimResized', {
+  pattern = '*',
+  command = 'wincmd =',
 })
 
 local commands = {
-  E = "e",
-  W = "w",
-  Wq = "wq",
-  WQ = "wq",
-  Wa = "wa",
-  WA = "wa",
-  WAq = "waq",
-  WAQ = "waq",
-  Waq = "waq",
-  WQa = "wqa",
-  Wqa = "wqa",
-  WQA = "wqa",
-  Q = "q",
-  QA = "qa",
-  Qa = "qa",
+  E = 'e',
+  W = 'w',
+  Wq = 'wq',
+  WQ = 'wq',
+  Wa = 'wa',
+  WA = 'wa',
+  WAq = 'waq',
+  WAQ = 'waq',
+  Waq = 'waq',
+  WQa = 'wqa',
+  Wqa = 'wqa',
+  WQA = 'wqa',
+  Q = 'q',
+  QA = 'qa',
+  Qa = 'qa',
 }
 
 for cmd, action in pairs(commands) do
-  vim.api.nvim_create_user_command(cmd, action .. "<bang> <args>", {
+  vim.api.nvim_create_user_command(cmd, action .. '<bang> <args>', {
     bang = true,
-    nargs = "?",
-    complete = "file",
+    nargs = '?',
+    complete = 'file',
   })
 end
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "qf",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
   callback = function()
-    vim.keymap.set("n", "j", "j^", { noremap = true, buffer = true })
-    vim.keymap.set("n", "k", "k^", { noremap = true, buffer = true })
-    vim.keymap.set("n", "<C-n>", "j^", { buffer = true })
-    vim.keymap.set("n", "<C-p>", "k^", { buffer = true })
-    vim.keymap.set("n", "<Down>", "j^", { buffer = true })
-    vim.keymap.set("n", "<Up>", "k^", { buffer = true })
+    vim.keymap.set('n', 'j', 'j^', { noremap = true, buffer = true })
+    vim.keymap.set('n', 'k', 'k^', { noremap = true, buffer = true })
+    vim.keymap.set('n', '<C-n>', 'j^', { buffer = true })
+    vim.keymap.set('n', '<C-p>', 'k^', { buffer = true })
+    vim.keymap.set('n', '<Down>', 'j^', { buffer = true })
+    vim.keymap.set('n', '<Up>', 'k^', { buffer = true })
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd('FileType', {
   pattern = {
-    "help",
-    "startuptime",
-    "qf",
-    "lspinfo",
-    "man",
-    "checkhealth",
+    'help',
+    'startuptime',
+    'qf',
+    'lspinfo',
+    'man',
+    'checkhealth',
   },
   command = [[
             nnoremap <buffer><silent> <C-c> :close<CR>
@@ -58,20 +58,33 @@ vim.api.nvim_create_autocmd("FileType", {
         ]],
 })
 
-vim.api.nvim_create_autocmd("WinEnter", {
+vim.api.nvim_create_autocmd('WinEnter', {
   callback = function()
     local function is_floating_win(win)
-      return vim.api.nvim_win_get_config(win).relative ~= ""
+      return vim.api.nvim_win_get_config(win).relative ~= ''
     end
     local win = vim.api.nvim_get_current_win()
     if is_floating_win(win) then
       local buffer = vim.api.nvim_win_get_buf(win)
-      vim.keymap.set("n", "<left>", "hzh", { buffer = buffer })
-      vim.keymap.set("n", "<right>", "lzl", { buffer = buffer })
-      vim.keymap.set("n", "<up>", "<c-y>k", { buffer = buffer })
-      vim.keymap.set("n", "<down>", "<c-e>", { buffer = buffer })
-      vim.keymap.set("n", "<s-left>", "zHg0", { buffer = buffer })
-      vim.keymap.set("n", "<s-right>", "zL", { buffer = buffer })
+      vim.keymap.set('n', '<left>', 'hzh', { buffer = buffer })
+      vim.keymap.set('n', '<right>', 'lzl', { buffer = buffer })
+      vim.keymap.set('n', '<up>', '<c-y>k', { buffer = buffer })
+      vim.keymap.set('n', '<down>', '<c-e>', { buffer = buffer })
+      vim.keymap.set('n', '<s-left>', 'zHg0', { buffer = buffer })
+      vim.keymap.set('n', '<s-right>', 'zL', { buffer = buffer })
     end
+  end,
+})
+
+vim.api.nvim_create_autocmd('ExitPre', {
+  callback = function()
+    vim.fn.jobstart('killall prettierd', { detach = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'Folded', { link = 'Normal' })
   end,
 })
