b0cc7203ba4cb231d83429bb60ba22f5be25d06c
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-11 15:54:49 -0700

    feat(blink.cmp.lua): improve copilot integration and disable auto brackets
    This commit improves the integration with copilot by automatically
    accepting suggestions and exiting insert mode. It also disables auto
    brackets in the accept options.

--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -1,54 +1,57 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "zbirenbaum/copilot.lua", "nvim-tree/nvim-web-devicons", "onsails/lspkind.nvim" },
-  event = "InsertEnter",
-  version = "*",
+  'saghen/blink.cmp',
+  dependencies = { 'zbirenbaum/copilot.lua', 'nvim-tree/nvim-web-devicons', 'onsails/lspkind.nvim' },
+  event = 'InsertEnter',
+  version = '*',
   opts = {
     enabled = function()
       -- print(vim.bo.filetype)
       -- print(vim.bo.buftype)
-      return vim.bo.buftype ~= "prompt"
+      return vim.bo.buftype ~= 'prompt'
         and vim.b.completion ~= false
-        and vim.bo.filetype ~= "grug-far"
-        and vim.bo.filetype ~= "norg"
+        and vim.bo.filetype ~= 'grug-far'
+        and vim.bo.filetype ~= 'norg'
       -- and vim.bo.filetype ~= "qf"
     end,
     keymap = {
-      preset = "super-tab",
-      ["<C-space>"] = { "show", "hide" },
-      ["<C-d>"] = { "select_next" },
-      ["<C-u>"] = { "select_prev" },
-      ["<C-e>"] = {},
-      ["<tab>"] = {
+      preset = 'super-tab',
+      ['<C-space>'] = { 'show', 'hide' },
+      ['<C-d>'] = { 'select_next' },
+      ['<C-u>'] = { 'select_prev' },
+      ['<C-e>'] = {},
+      ['<tab>'] = {
         function(cmp)
           if cmp.accept() then
             return true
           end
-          local copilotSuggestion = require("copilot.suggestion")
+          local copilotSuggestion = require('copilot.suggestion')
           if copilotSuggestion.is_visible() then
             copilotSuggestion.accept()
+            vim.schedule(function()
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
+            end)
             return true
           end
         end,
-        "fallback",
+        'fallback',
       },
-      ["<CR>"] = {
+      ['<CR>'] = {
         function(cmp)
           return cmp.accept()
         end,
-        "fallback",
+        'fallback',
       },
-      ["<Right>"] = {
+      ['<Right>'] = {
         function(cmp)
           return cmp.accept()
         end,
-        "fallback",
+        'fallback',
       },
-      ["<c-c>"] = {
+      ['<c-c>'] = {
         function(cmp)
           cmp.cancel()
           vim.schedule(function()
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "n", true)
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
           end)
         end,
       },
@@ -71,12 +74,12 @@ return {
       ghost_text = { enabled = false },
       menu = {
         auto_show = true,
-        border = "single",
+        border = 'single',
         draw = {
-          treesitter = { "lsp" },
+          treesitter = { 'lsp' },
           columns = {
-            { "label" },
-            { "kind_icon", "kind", "label_description", gap = 1 },
+            { 'label' },
+            { 'kind_icon', 'kind', 'label_description', gap = 1 },
           },
           components = {
             label = { ellipsis = true, width = { max = 16, fill = false } },
@@ -85,16 +88,16 @@ return {
             kind_icon = {
               width = { fill = false },
               text = function(ctx)
-                local lspkind = require("lspkind")
+                local lspkind = require('lspkind')
                 local icon = ctx.kind_icon
-                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
                   if dev_icon then
                     icon = dev_icon
                   end
                 else
-                  icon = require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
+                  icon = require('lspkind').symbolic(ctx.kind, {
+                    mode = 'symbol',
                   })
                 end
 
@@ -102,8 +105,8 @@ return {
               end,
               highlight = function(ctx)
                 local hl = ctx.kind_hl
-                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
                   if dev_icon then
                     hl = dev_hl
                   end
@@ -115,32 +118,32 @@ return {
         },
         scrollbar = false,
       },
-      accept = { auto_brackets = { enabled = true } },
+      accept = { auto_brackets = { enabled = false } },
     },
     signature = {
       enabled = true,
       window = {
-        border = "single",
+        border = 'single',
         show_documentation = false,
         scrollbar = true,
       },
     },
     appearance = {
       use_nvim_cmp_as_default = false,
-      nerd_font_variant = "mono",
+      nerd_font_variant = 'mono',
     },
-    fuzzy = { implementation = "rust" },
+    fuzzy = { implementation = 'rust' },
     sources = {
       providers = {
         lsp = {
           async = true,
         },
       },
-      default = { "lsp", "buffer" },
+      default = { 'lsp', 'buffer' },
       per_filetype = {
-        codecompanion = { "codecompanion", "lsp", "buffer" },
+        codecompanion = { 'codecompanion', 'lsp', 'buffer' },
       },
     },
   },
-  opts_extend = { "sources.default" },
+  opts_extend = { 'sources.default' },
 }
