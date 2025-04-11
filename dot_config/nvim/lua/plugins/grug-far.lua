88f4afe7387271b59ba3a4f17afc9f153a8d07ac
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-11 15:53:13 -0700

    feat(grug-far): improve grug-far mappings and configuration
    This commit enhances the grug-far plugin configuration for improved
    usability and functionality:
    
    - Updates key mappings for better navigation and command execution.
    - Adds file path concealment for cleaner display.
    - Enables ripgrep and astgrep-rules engines.

--- dot_config/nvim/lua/plugins/grug-far.lua
+++ dot_config/nvim/lua/plugins/grug-far.lua
@@ -1,94 +1,89 @@
 local function get_cwd()
-  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel 2>/dev/null")[1]
+  local git_root = vim.fn.systemlist('git rev-parse --show-toplevel 2>/dev/null')[1]
   if vim.v.shell_error ~= 0 then
-    return ""
+    return ''
   end
-  local slash_count = select(2, vim.fn.getcwd():gsub("^" .. vim.pesc(git_root), ""):gsub("/", "/"))
-  return slash_count > 0 and (".." .. ("/.."):rep(slash_count - 1)) or ""
+  local slash_count = select(2, vim.fn.getcwd():gsub('^' .. vim.pesc(git_root), ''):gsub('/', '/'))
+  return slash_count > 0 and ('..' .. ('/..'):rep(slash_count - 1)) or ''
 end
 
 return {
-  "MagicDuck/grug-far.nvim",
+  'MagicDuck/grug-far.nvim',
   keys = {
     {
-      "<leader>gf",
+      '<leader>gf',
       function()
-        require("grug-far").open({ prefills = { paths = get_cwd() } })
+        require('grug-far').open({ prefills = { paths = get_cwd() } })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>gfb",
+      '<leader>gfb',
       function()
-        require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+        require('grug-far').open({ prefills = { paths = vim.fn.expand('%') } })
       end,
-      mode = { "n", "x" },
+      mode = { 'n', 'x' },
     },
     {
-      "<leader>gfw",
+      '<leader>gfw',
       function()
-        require("grug-far").open({
+        require('grug-far').open({
           prefills = {
             paths = get_cwd(),
-            flags = "--case-sensitive",
-            search = "\\b" .. vim.fn.expand("<cword>") .. "\\b",
+            flags = '--case-sensitive',
+            search = '\\b' .. vim.fn.expand('<cword>') .. '\\b',
           },
         })
       end,
-      mode = { "n" },
+      mode = { 'n' },
     },
     {
-      "<leader>gfwb",
+      '<leader>gfwb',
       function()
-        require("grug-far").open({
+        require('grug-far').open({
           prefills = {
-            flags = "--case-sensitive",
-            search = "\\b" .. vim.fn.expand("<cword>") .. "\\b",
-            paths = vim.fn.expand("%"),
+            flags = '--case-sensitive',
+            search = '\\b' .. vim.fn.expand('<cword>') .. '\\b',
+            paths = vim.fn.expand('%'),
           },
         })
       end,
-      mode = { "n" },
+      mode = { 'n' },
     },
     {
-      "<leader>gfv",
+      '<leader>gfv',
       function()
-        require("grug-far").open({
+        require('grug-far').open({
           prefills = {
-            search = vim.fn.getreg("/"),
-            flags = "--case-sensitive",
+            search = vim.fn.getreg('/'),
+            flags = '--case-sensitive',
             paths = get_cwd(),
           },
         })
       end,
-      mode = { "x" },
+      mode = { 'x' },
     },
     {
-      "<leader>gfvb",
+      '<leader>gfvb',
       function()
-        require("grug-far").open({
+        require('grug-far').open({
           prefills = {
-            search = vim.fn.getreg("/"),
-            flags = "--case-sensitive",
-            paths = vim.fn.expand("%"),
+            search = vim.fn.getreg('/'),
+            flags = '--case-sensitive',
+            paths = vim.fn.expand('%'),
           },
         })
       end,
-      mode = { "x" },
+      mode = { 'x' },
     },
   },
   config = function(_, opts)
-    require("grug-far").setup(opts)
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "grug-far",
+    require('grug-far').setup(opts)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'grug-far',
       callback = function()
-        vim.opt.signcolumn = "no"
-        vim.keymap.set({ "i" }, "<Enter>", "<Esc>", { buffer = true })
-        vim.keymap.set({ "n", "x" }, "q", function()
-          require("grug-far").close_instance()
-        end, { buffer = true })
-        -- vim.keymap.set({ "n" }, "o", "<Nop>", { noremap = true, buffer = true })
-        -- vim.keymap.set({ "n" }, "O", "<Nop>", { noremap = true, buffer = true })
+        vim.opt.signcolumn = 'no'
+        -- vim.keymap.set({ 'i' }, '<Enter>', '<Esc>', { buffer = true })
       end,
     })
   end,
@@ -104,9 +99,9 @@ return {
       abort = false,
       applyNext = false,
       applyPrev = false,
-      close = { n = "<c-c>" },
-      gotoLocation = { n = "<enter>" },
-      help = { n = "?" },
+      close = { n = '<c-c>' },
+      gotoLocation = { n = '<enter>' },
+      help = { n = '?' },
       historyAdd = false,
       historyOpen = false,
       openLocation = false,
@@ -114,13 +109,13 @@ return {
       openPrevLocation = false,
       pickHistoryEntry = false,
       previewLocation = false,
-      qflist = { n = "<C-q>" },
-      refresh = { n = "R" },
-      replace = { n = "<localleader>r" },
-      swapEngine = false,
+      qflist = { n = '<C-q>' },
+      refresh = { n = 'R' },
+      replace = { n = '<localleader>rr' },
+      swapEngine = { n = '<localleader>ee' },
       swapReplacementInterpreter = false,
-      syncLine = { n = "<localleader>l" },
-      syncLocations = { n = "<localleader>s" },
+      syncLine = false,
+      syncLocations = false,
       toggleShowCommand = false,
     },
     filePathConceal = function(params)
@@ -135,7 +130,8 @@ return {
 
       return first_part_len, first_part_len + delta
     end,
-
+    filePathConcealChar = '…',
+    enabledEngines = { 'ripgrep', 'astgrep-rules' },
     -- engines = {
     --   ripgrep = {
     --     extraArgs = "",
