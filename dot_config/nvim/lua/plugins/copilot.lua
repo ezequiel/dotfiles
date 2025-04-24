e491c276a608f89f70e83b0c075638fc2e75be9e
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-24 15:17:27 -0700

    feat(copilot.lua): set copilot model to gemini-2.5-pro

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
-  "zbirenbaum/copilot.lua",
-  event = "VeryLazy",
+  'zbirenbaum/copilot.lua',
+  event = 'VeryLazy',
   opts = {
     suggestion = {
       enabled = true,
@@ -10,26 +10,12 @@ return {
         accept = false,
         accept_word = false,
         accept_line = false,
-        next = "<C-n>",
-        prev = "<C-p>",
-        dismiss = "<C-c>",
+        next = '<C-n>',
+        prev = '<C-p>',
+        dismiss = '<C-c>',
       },
     },
     panel = { enabled = false },
-    -- init = function()
-    --   vim.api.nvim_create_autocmd("User", {
-    --     pattern = "BlinkCmpMenuOpen",
-    --     callback = function()
-    --       require("copilot.suggestion").dismiss()
-    --       vim.b.copilot_suggestion_hidden = true
-    --     end,
-    --   })
-    --   vim.api.nvim_create_autocmd("User", {
-    --     pattern = "BlinkCmpMenuClose",
-    --     callback = function()
-    --       vim.b.copilot_suggestion_hidden = false
-    --     end,
-    --   })
-    -- end,
+    copilot_model = 'gemini-2.5-pro',
   },
 }
