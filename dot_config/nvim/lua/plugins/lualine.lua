953f88efb0623cd01aeea1acbcd413e3cf39f50d
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-24 19:39:13 -0700

    feat(lualine): add toggle function to hide/show lualine status bar

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,17 @@
 local is_hidden = true
 return {
-  "nvim-lualine/lualine.nvim",
+  'nvim-lualine/lualine.nvim',
   keys = {
     {
-      "<leader>ll",
+      '<leader>ll',
       function()
         if is_hidden then
-          require("lualine").hide({ unhide = true })
+          require('lualine').hide({ unhide = true })
           vim.opt.laststatus = 3
           is_hidden = false
         else
           vim.opt.laststatus = 0
-          require("lualine").hide()
+          require('lualine').hide()
           is_hidden = true
         end
       end,
@@ -19,31 +19,31 @@ return {
   },
   opts = {
     options = {
-      theme = "tokyonight",
+      theme = 'tokyonight',
       icons_enabled = false,
-      section_separators = { left = "", right = "" },
+      section_separators = { left = '', right = '' },
     },
     sections = {
-      lualine_a = { "mode" },
+      lualine_a = { 'mode' },
       lualine_c = {
-        "diff",
+        'diff',
         {
-          "diagnostics",
-          sources = { "nvim_diagnostic" },
-          symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          'diagnostics',
+          sources = { 'nvim_diagnostic' },
+          symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
         },
-        "searchcount",
+        'searchcount',
       },
       lualine_b = {},
       lualine_x = {},
       lualine_y = {
-        "branch",
+        'branch',
       },
-      lualine_z = { "lsp_status" },
+      lualine_z = { 'lsp_status' },
     },
   },
   config = function(_, opts)
-    local lualine = require("lualine")
+    local lualine = require('lualine')
     lualine.setup(opts)
     lualine.hide()
     vim.opt.laststatus = 0
