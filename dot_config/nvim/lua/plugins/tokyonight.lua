013beb4090ce3c695d71c95e5942370588375f13
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-24 19:30:20 -0700

    feat(tokyonight.lua): configure tokyonight theme and statusline

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,15 +1,20 @@
 return {
-  "folke/tokyonight.nvim",
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "night",
+    require('tokyonight').setup({
+      style = 'night',
       lualine_bold = true,
       plugins = {
         auto = true,
       },
     })
-    vim.cmd("colorscheme tokyonight-night")
+  end,
+  init = function()
+    vim.cmd('colorscheme tokyonight-night')
+    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#1a1b26' })
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#16161e', bg = '#1a1b26' })
+    vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#16161e', bg = '#1a1b26' })
   end,
 }
