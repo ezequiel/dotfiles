262572adb145ed56e3da6492d92e99a01c2fde97
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-04-26 23:14:00 -0700

    feat: remove guicursor option to use system cursor

--- dot_config/nvim/lua/config/opt.lua
+++ dot_config/nvim/lua/config/opt.lua
@@ -50,8 +50,6 @@ vim.opt.signcolumn = 'yes:2'
 vim.opt.wrap = false
 vim.opt.errorbells = false
 vim.opt.visualbell = false
-vim.opt.guicursor =
-  'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
 vim.opt.numberwidth = 5
 vim.opt.shortmess:append('I')
 vim.opt.shortmess:append('F')
