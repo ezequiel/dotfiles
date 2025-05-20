fd27a33a86128509797ff45eec8b55787d66d237
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-05-19 21:30:00 -0700

    feat(nvim): configure mini.move plugin to use Cmd+Shift+j/k
    This commit configures the mini.move plugin to use Cmd+Shift+j/k
    for moving lines and selections up and down.

--- dot_config/nvim/lua/plugins/mini.move.lua
+++ dot_config/nvim/lua/plugins/mini.move.lua
@@ -1,25 +1,25 @@
 return {
-  "echasnovski/mini.move",
-  version = "*",
+  'echasnovski/mini.move',
+  version = '*',
   keys = {
-    -- { "<S-j>", "<cmd>lua MiniMove.move_line('down')<cr>", mode = { "n" } },
-    -- { "<S-k>", "<cmd>lua MiniMove.move_line('up')<cr>", mode = { "n" } },
-    { "J", "<cmd>lua MiniMove.move_selection('down')<cr>", mode = { "x" } },
-    { "K", "<cmd>lua MiniMove.move_selection('up')<cr>", mode = { "x" } },
+    { '<D-S-j>', "<cmd>lua MiniMove.move_line('down')<cr>", mode = { 'n' } },
+    { '<D-S-k>', "<cmd>lua MiniMove.move_line('up')<cr>", mode = { 'n' } },
+    { '<D-S-j>', "<cmd>lua MiniMove.move_selection('down')<cr>", mode = { 'x' } },
+    { '<D-S-k>', "<cmd>lua MiniMove.move_selection('up')<cr>", mode = { 'x' } },
   },
   opts = {
     mappings = {
-      left = "",
-      right = "",
-      down = "",
-      up = "",
-      line_left = "",
-      line_right = "",
-      line_down = "",
-      line_up = "",
+      left = '',
+      right = '',
+      down = '',
+      up = '',
+      line_left = '',
+      line_right = '',
+      line_down = '',
+      line_up = '',
     },
   },
   config = function(_, opts)
-    require("mini.move").setup(opts)
+    require('mini.move').setup(opts)
   end,
 }
