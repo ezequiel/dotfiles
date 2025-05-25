c5faf8da3839d3ea6cd64a348e521c05f73d8502
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-05-24 17:45:13 -0700

    feat(chezmoi.lua): disable notifications for chezmoi plugin events

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,26 +1,38 @@
 return {
-  event = "VeryLazy",
-  "xvzc/chezmoi.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  event = 'VeryLazy',
+  'xvzc/chezmoi.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
     edit = {
       watch = true,
       force = true,
     },
-    notification = {
-      on_open = false,
-      on_apply = false,
-      on_watch = false,
+    events = {
+      on_open = {
+        notification = {
+          enable = false,
+        },
+      },
+      on_watch = {
+        notification = {
+          enable = false,
+        },
+      },
+      on_apply = {
+        notification = {
+          enable = false,
+        },
+      },
     },
   },
   init = function()
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
       pattern = {
-        (vim.fn.expand("~")):gsub("\\", "/") .. "/.local/share/chezmoi/*",
+        (vim.fn.expand('~')):gsub('\\', '/') .. '/.local/share/chezmoi/*',
       },
       callback = function(event)
         vim.schedule(function()
-          require("chezmoi.commands.__edit").watch(event.buf)
+          require('chezmoi.commands.__edit').watch(event.buf)
         end)
       end,
     })
