16adedc8f2bb08efa7f514ec8ea3c9b74fc3c818
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-05-26 16:50:12 -0700

    feat(fidget.lua): configure fidget.nvim plugin with custom poll rates and suppress on insert option

--- dot_config/nvim/lua/plugins/fidget.lua
+++ dot_config/nvim/lua/plugins/fidget.lua
@@ -1,4 +1,12 @@
 return {
   'j-hui/fidget.nvim',
-  opts = {},
+  opts = {
+    progress = {
+      poll_rate = 20,
+      suppress_on_insert = true,
+    },
+    notification = {
+      poll_rate = 20,
+    },
+  },
 }
