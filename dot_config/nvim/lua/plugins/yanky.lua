e670dd01bc8aeec6ae376dcc9324d213e08a6ad6
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-05-29 20:01:16 -0700

    feat(yanky.lua): add snacks.nvim as dependency and add keybind to open yank history

--- dot_config/nvim/lua/plugins/yanky.lua
+++ dot_config/nvim/lua/plugins/yanky.lua
@@ -1,14 +1,20 @@
 return {
-  "gbprod/yanky.nvim",
+  'gbprod/yanky.nvim',
   dependencies = {
-    { "kkharji/sqlite.lua" },
+    'folke/snacks.nvim',
   },
-  opts = {
-    ring = { storage = "sqlite" },
-  },
+  opts = {},
   keys = {
-    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+    { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank text' },
+    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put yanked text after cursor' },
+    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put yanked text before cursor' },
+    {
+      '<leader>yh',
+      function()
+        Snacks.picker.yanky()
+      end,
+      mode = { 'n', 'x' },
+      desc = 'Open Yank History',
+    },
   },
 }
