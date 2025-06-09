5ddbdfb7e322dd0ca84a209f07edc190898083aa
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-06-09 15:29:51 -0700

    feat(no-neck-pain): enable plugin on tab enter and vim enter

--- dot_config/nvim/lua/plugins/no-neck-pain.lua
+++ dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,11 +1,10 @@
 return {
   lazy = false,
   'shortcuts/no-neck-pain.nvim',
-  commit = 'c824c54d837160ac32d0f877574ccfa2ecce9947',
   opts = {
     autocmds = {
-      -- enableOnTabEnter = true,
-      -- enableOnVimEnter = true,
+      enableOnTabEnter = true,
+      enableOnVimEnter = true,
       skipEnteringNoNeckPainBuffer = true,
     },
     mappings = {
@@ -20,13 +19,4 @@ return {
       },
     },
   },
-  init = function()
-    vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-      pattern = '*',
-      once = true,
-      callback = function()
-        require('no-neck-pain.main').enable('')
-      end,
-    })
-  end,
 }
