76e00dc2912fe1bc307edfeab8abb7875c96c7bb
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-06-17 00:24:23 -0700

    feat(nvim-treesitter-context): enable plugin configuration

--- dot_config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -2,6 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter-context',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   event = 'VeryLazy',
+  config = true,
   init = function()
     vim.keymap.set('n', '<leader>gc', function()
       require('treesitter-context').go_to_context(vim.v.count1)
