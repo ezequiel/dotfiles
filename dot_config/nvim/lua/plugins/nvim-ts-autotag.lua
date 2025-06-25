9dc7fe4d72772ab8b59615ac6008c7088d5a942f
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-06-25 00:28:37 -0700

    refactor(nvim-ts-autotag): remove autopairs dependency and simplify options for nvim-ts-autotag plugin

--- dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,17 +1,11 @@
 return {
   'windwp/nvim-ts-autotag',
-  -- dependencies = { 'windwp/nvim-autopairs' },
   event = 'InsertEnter',
   opts = {
     disable_filetype = { 'snacks_picker_input', 'grug-far' },
     enabled = function()
-      -- print(vim.bo.buftype)
-      -- print(vim.bo.filetype)
       return vim.bo.buftype ~= 'prompt' and vim.b.completion ~= false
     end,
   },
   config = true,
-  -- config = function(_, opts)
-  -- require('nvim-autopairs').setup(opts)
-  -- end,
 }
