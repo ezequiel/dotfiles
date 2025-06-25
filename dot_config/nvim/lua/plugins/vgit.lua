6f9c2bf6ff0e8cb7f964aff85a886ed89f72cff4
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-06-25 00:28:07 -0700

    refactor(vgit.lua): migrate vgit keymaps to use the 'keys' table for better readability and maintainability

--- dot_config/nvim/lua/plugins/vgit.lua
+++ dot_config/nvim/lua/plugins/vgit.lua
@@ -1,42 +1,105 @@
 return {
   'tanvirtin/vgit.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
   lazy = false,
-  config = function()
-    require('vgit').setup({
-      keymaps = {
-        ['n [h'] = 'hunk_up',
-        ['n ]h'] = 'hunk_down',
-        ['n <leader>hs'] = 'buffer_hunk_stage',
-        ['n <leader>hr'] = 'buffer_hunk_reset',
-        ['n <leader>hp'] = 'buffer_hunk_preview',
-        ['n <leader>hR'] = 'buffer_reset',
-        ['n <leader>hb'] = 'buffer_blame_preview',
-        ['n <leader>sp'] = 'project_stash_preview',
-        ['n <leader>lp'] = 'project_logs_preview',
-        ['n <leader>lb'] = 'toggle_live_blame',
-        ['n <leader>dp'] = function()
-          local state = require('no-neck-pain.state')
-          if state.has_tabs(state) and state.is_active_tab_registered(state) then
-            require('no-neck-pain').disable()
-          end
-          require('vgit').project_diff_preview()
-        end,
-        -- ['n <leader>bdp'] = 'buffer_diff_preview',
-        -- ['n <leader>bhp'] = 'buffer_history_preview',
-        -- ['n <leader>gx'] = function()
-        --   require('vgit').toggle_diff_preference()
-        -- end,
+  keys = {
+    {
+      '[h',
+      mode = 'n',
+      function()
+        require('vgit').hunk_up()
+      end,
+    },
+    {
+      ']h',
+      mode = 'n',
+      function()
+        require('vgit').hunk_down()
+      end,
+    },
+    {
+      '<leader>hs',
+      mode = 'n',
+      function()
+        require('vgit').buffer_hunk_stage()
+      end,
+    },
+    {
+      '<leader>hr',
+      mode = 'n',
+      function()
+        require('vgit').buffer_hunk_reset()
+      end,
+    },
+    {
+      '<leader>hp',
+      mode = 'n',
+      function()
+        require('vgit').buffer_hunk_preview()
+      end,
+    },
+    {
+      '<leader>hR',
+      mode = 'n',
+      function()
+        require('vgit').buffer_reset()
+      end,
+    },
+    {
+      '<leader>hb',
+      mode = 'n',
+      function()
+        require('vgit').buffer_blame_preview()
+      end,
+    },
+    {
+      '<leader>sp',
+      mode = 'n',
+      function()
+        require('vgit').project_stash_preview()
+      end,
+    },
+    {
+      '<leader>lp',
+      mode = 'n',
+      function()
+        require('vgit').project_logs_preview()
+      end,
+    },
+    {
+      '<leader>lb',
+      mode = 'n',
+      function()
+        require('vgit').toggle_live_blame()
+      end,
+    },
+    {
+      '<leader>dp',
+      mode = 'n',
+      function()
+        local state = require('no-neck-pain.state')
+        if state.has_tabs(state) and state.is_active_tab_registered(state) then
+          require('no-neck-pain').disable()
+        end
+        require('vgit').project_diff_preview()
+      end,
+    },
+    -- { '<leader>bdp', mode = 'n', function() require('vgit').buffer_diff_preview() end },
+    -- { '<leader>bhp', mode = 'n', function() require('vgit').buffer_history_preview() end },
+    -- { '<leader>gx', mode = 'n', function() require('vgit').toggle_diff_preference() end },
+  },
+  opts = {
+    settings = {
+      hls = {
+        GitAppBar = 'QuickfixLine',
+        GitHeader = 'Keyword',
       },
-      settings = {
-        hls = {
-          GitAppBar = 'QuickfixLine',
-          GitHeader = 'Keyword',
-        },
-        signs = {
-          priority = 1337,
-        },
+      signs = {
+        priority = 1337,
       },
-    })
-  end,
+    },
+  },
 }
