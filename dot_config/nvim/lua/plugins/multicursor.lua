98a91a5e4b2a6ee34cc27f82c6098c464faa446e
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-05 14:20:05 -0700

    feat(multicursor.lua): refactor multicursor plugin with keymaps and opts

--- dot_config/nvim/lua/plugins/multicursor.lua
+++ dot_config/nvim/lua/plugins/multicursor.lua
@@ -1,50 +1,66 @@
 return {
-  event = 'VeryLazy',
   'jake-stewart/multicursor.nvim',
-  -- branch = '1.0',
   commit = 'c117b3fc537cda262a06638ca88fe6de1b53ab59',
-  config = function()
+  keys = {
+    {
+      '<D-Up>',
+      mode = { 'n', 'x' },
+      function()
+        require('multicursor-nvim').lineAddCursor(-1, { skipEmpty = false })
+      end,
+    },
+    {
+      '<D-Down>',
+      mode = { 'n', 'x' },
+      function()
+        require('multicursor-nvim').lineAddCursor(1, { skipEmpty = false })
+      end,
+    },
+    {
+      '<D-S-Up>',
+      mode = { 'n', 'x' },
+      function()
+        require('multicursor-nvim').lineSkipCursor(-1, { skipEmpty = false })
+      end,
+    },
+    {
+      '<D-S-Down>',
+      mode = { 'n', 'x' },
+      function()
+        require('multicursor-nvim').lineSkipCursor(1, { skipEmpty = false })
+      end,
+    },
+    {
+      '<C-a>',
+      mode = { 'n', 'x' },
+      function()
+        require('multicursor-nvim').toggleCursor()
+      end,
+    },
+    {
+      'I',
+      mode = 'x',
+      function()
+        require('multicursor-nvim').insertVisual()
+      end,
+    },
+    {
+      'A',
+      mode = 'x',
+      function()
+        require('multicursor-nvim').appendVisual()
+      end,
+    },
+  },
+  opts = {},
+  config = function(_, opts)
     local mc = require('multicursor-nvim')
-    mc.setup()
-
-    local set = vim.keymap.set
-
-    -- Add or skip cursor above/below the main cursor.
-    set({ 'n', 'x' }, '<D-Up>', function()
-      mc.lineAddCursor(-1, { skipEmpty = false })
-    end)
-    set({ 'n', 'x' }, '<D-Down>', function()
-      mc.lineAddCursor(1, { skipEmpty = false })
-    end)
-    set({ 'n', 'x' }, '<D-S-Up>', function()
-      mc.lineSkipCursor(-1, { skipEmpty = false })
-    end)
-    set({ 'n', 'x' }, '<D-S-Down>', function()
-      mc.lineSkipCursor(1, { skipEmpty = false })
-    end)
-
-    -- Disable and enable cursors.
-    set({ 'n', 'x' }, '<c-a>', function()
-      mc.toggleCursor()
-    end)
-
-    -- Append/insert for each line of visual selections.
-    -- Similar to block selection insertion.
-    set('x', 'I', mc.insertVisual)
-    set('x', 'A', mc.appendVisual)
-
-    -- Mappings defined in a keymap layer only apply when there are
-    -- multiple cursors. This lets you have overlapping mappings.
-    mc.addKeymapLayer(function(layerSet)
-      -- Select a different cursor as the main one.
-      layerSet({ 'n', 'x' }, '<left>', mc.prevCursor)
-      layerSet({ 'n', 'x' }, '<right>', mc.nextCursor)
-
-      -- Delete the main cursor.
-      layerSet({ 'n', 'x' }, '<leader>x', mc.deleteCursor)
-
-      -- Enable and clear cursors using escape.
-      layerSet('n', '<esc>', function()
+    mc.setup(opts)
+    mc.addKeymapLayer(function(set)
+      set({ 'n', 'x' }, '<left>', mc.prevCursor)
+      set({ 'n', 'x' }, '<right>', mc.nextCursor)
+      set({ 'n', 'x' }, '<leader>x', mc.deleteCursor)
+      set('n', '<esc>', function()
         if not mc.cursorsEnabled() then
           mc.enableCursors()
         else
