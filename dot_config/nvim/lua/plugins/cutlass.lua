2fca46a933261bc19dca5e8a058d5a226abb8685
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-06 03:48:40 -0700

    feat(cutlass.lua): add cutlass plugin keybindings for text manipulation.

--- dot_config/nvim/lua/plugins/cutlass.lua
+++ dot_config/nvim/lua/plugins/cutlass.lua
@@ -1,6 +1,16 @@
 return {
   'gbprod/cutlass.nvim',
-  event = 'VeryLazy',
+  keys = {
+    { 'C', mode = { 'n', 'x' } },
+    { 's', mode = { 'n', 'x' } },
+    { 'S', mode = { 'n', 'x' } },
+    { 'd', mode = { 'n', 'x' } },
+    { 'D', mode = { 'n', 'x' } },
+    { 'x', mode = { 'n', 'x' } },
+    { 'X', mode = { 'n', 'x' } },
+    { 'm', mode = { 'n', 'x' } },
+    { 'M', mode = { 'n', 'x' } },
+  },
   opts = {
     cut_key = 'm',
   },
