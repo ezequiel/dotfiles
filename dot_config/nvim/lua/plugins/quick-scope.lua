d2764dc71fe0b900c934246feb25753dec2a3bf2
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-06 15:23:25 -0700

    feat(quick-scope.lua): configure quick-scope plugin to highlight on fFtT keys.

--- dot_config/nvim/lua/plugins/quick-scope.lua
+++ dot_config/nvim/lua/plugins/quick-scope.lua
@@ -3,6 +3,7 @@ return {
   'unblevable/quick-scope',
   init = function()
     vim.g.qs_lazy_highlight = 1
+    vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
     vim.cmd("highlight QuickScopePrimary guifg='#00ffff' gui=bold,underline ctermfg=14 cterm=bold,underline")
     -- vim.cmd.highlight("QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
   end,
