e1ab09507d86ae9036aab6eba28b33402f2e9225
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-06 04:44:44 -0700

    feat: add quick-scope plugin for enhanced code navigation

---
+++ dot_config/nvim/lua/plugins/quick-scope.lua
@@ -0,0 +1,9 @@
+return {
+  event = 'CursorMoved',
+  'unblevable/quick-scope',
+  init = function()
+    vim.g.qs_lazy_highlight = 1
+    vim.cmd("highlight QuickScopePrimary guifg='#00ffff' gui=bold,underline ctermfg=14 cterm=bold,underline")
+    -- vim.cmd.highlight("QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
+  end,
+}
