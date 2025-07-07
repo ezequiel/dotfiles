4cd88cb9fb22ee1bb0c7f3a94636eaadbd39824b
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-07 01:22:18 -0700

    refactor(quick-scope): use local keys variable for quick-scope plugin configuration.

--- dot_config/nvim/lua/plugins/quick-scope.lua
+++ dot_config/nvim/lua/plugins/quick-scope.lua
@@ -1,10 +1,10 @@
+local keys = { 'f', 'F', 't', 'T' }
 return {
-  event = 'CursorMoved',
   'unblevable/quick-scope',
+  keys = keys,
   init = function()
     vim.g.qs_lazy_highlight = 1
-    vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    vim.g.qs_highlight_on_keys = keys
     vim.cmd("highlight QuickScopePrimary guifg='#00ffff' gui=bold,underline ctermfg=14 cterm=bold,underline")
-    -- vim.cmd.highlight("QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
   end,
 }
