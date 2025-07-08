b4b8eeb6b6e5eb830d325aaeb0432a7fd61b3181
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-07 17:15:56 -0700

    feat: remove smear-cursor plugin due to performance concerns

--- dot_config/nvim/lua/plugins/smear-cursor.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- 'sphamba/smear-cursor.nvim',
-  -- lazy = false,
-  -- opts = {
-  --   smear_vertically = false,
-  --   smear_diagonally = false,
-  --   smear_insert_mode = false,
-  -- },
-}
