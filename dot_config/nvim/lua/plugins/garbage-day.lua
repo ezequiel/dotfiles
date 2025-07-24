a105ff60ff7a1413779763439f36c310a0ed7e14
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-24 16:44:19 -0700

    feat: remove garbage-day plugin since it is no longer needed

--- dot_config/nvim/lua/plugins/garbage-day.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'zeioth/garbage-day.nvim',
-  dependencies = 'neovim/nvim-lspconfig',
-  lazy = false,
-  opts = {
-    -- aggressive_mode = true,
-    excluded_lsp_clients = { 'copilot' },
-  },
-}
