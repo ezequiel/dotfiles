670f26d568eebce4a1d1f99a65b46b7241dd5c78
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-07-30 22:16:01 -0700

    feat(nvim-ts-autotag): add AvanteInput to disabled filetypes.

--- dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -2,7 +2,7 @@ return {
   'windwp/nvim-ts-autotag',
   lazy = false,
   opts = {
-    disable_filetype = { 'snacks_picker_input', 'grug-far' },
+    disable_filetype = { 'snacks_picker_input', 'grug-far', 'AvanteInput' },
     enabled = function()
       return vim.bo.buftype ~= 'prompt' and vim.b.completion ~= false
     end,
