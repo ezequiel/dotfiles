7b5a5b3a83b36f7fd97b318eff6ffee9385aa6a3
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-08-09 02:15:45 -0700

    feat(nvim): disable netrw plugin and deprecate function to improve nvim performance.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = '\\'
 vim.g.maplocalleader = '\\'
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwPlugin = 0
 vim.deprecate = function() end
 
 require('config.opt')
