f4c40f1793688a909ba3054155300585ea2d4343
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-08-12 01:04:23 -0700

    refactor: remove typescript-language-server and update typescript-tools

--- dot_config/nvim/lua/plugins/typescript-tools.lua
+++ dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -6,14 +6,16 @@ return {
     settings = {
       publish_diagnostic_on = 'change',
       expose_as_code_action = { 'fix_all', 'add_missing_imports', 'remove_unused_imports' },
-      tsserver_path = vim.env.XDG_DATA_HOME
-        .. '/mise/installs/npm-typescript-language-server/latest/bin/typescript-language-server',
+      -- tsserver_path = vim.env.XDG_DATA_HOME
+      --   .. '/mise/installs/npm-typescript-language-server/latest/bin/typescript-language-server',
       jsx_close_tag = {
         enable = true,
         filetypes = { 'javascriptreact', 'typescriptreact' },
       },
       tsserver_format_options = {},
-      tsserver_file_preferences = {},
+      tsserver_file_preferences = {
+        -- importModuleSpecifierPreference = 'non-relative',
+      },
     },
   },
 }
