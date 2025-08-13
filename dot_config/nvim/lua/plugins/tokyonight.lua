c74783dca3d5b99c151e149380f051ecbc35e777
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-08-13 01:05:20 -0700

    feat(tokyonight.lua): configure tokyonight with opts and remove -night suffix.

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -2,16 +2,14 @@ return {
   'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
-  config = function()
-    require('tokyonight').setup({
-      style = 'night',
-      plugins = {
-        auto = true,
-      },
-    })
-  end,
+  opts = {
+    style = 'night',
+    plugins = {
+      auto = true,
+    },
+  },
   init = function()
-    vim.cmd('colorscheme tokyonight-night')
+    vim.cmd('colorscheme tokyonight')
     vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#1a1b26' })
     vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#16161e', bg = '#1a1b26' })
     vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#16161e', bg = '#1a1b26' })
