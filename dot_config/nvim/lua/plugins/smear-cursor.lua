0f1bc54e0684f42032f65716cb5333e7656abfd5
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-08-28 23:33:31 -0700

    feat: add smear-cursor plugin to enhance cursor visibility and aesthetics

---
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  lazy = false,
+  opts = {
+    smear_between_buffers = false,
+    smear_vertically = false,
+    smear_diagonally = false,
+    smear_insert_mode = false,
+  },
+}
