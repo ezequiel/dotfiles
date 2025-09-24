eca93d796cebfdff2010e7aee5c65380f36d2edb
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-09-24 12:44:54 -0700

    feat: add opencode plugin with keybindings and configuration options.

---
+++ dot_config/nvim/lua/plugins/opencode.lua
@@ -0,0 +1,76 @@
+return {
+  'NickvanDyke/opencode.nvim',
+  dependencies = {
+    'folke/snacks.nvim',
+  },
+  keys = {
+    {
+      '<leader>ot',
+      function()
+        require('opencode').toggle()
+      end,
+    },
+    {
+      '<leader>oA',
+      function()
+        require('opencode').ask()
+      end,
+    },
+    {
+      '<leader>oa',
+      function()
+        require('opencode').ask('@cursor: ')
+      end,
+    },
+    {
+      '<leader>oa',
+      mode = { 'x' },
+      function()
+        require('opencode').ask('@selection: ')
+      end,
+    },
+    {
+      '<leader>on',
+      function()
+        require('opencode').command('session_new')
+      end,
+    },
+    {
+      '<leader>oy',
+      function()
+        require('opencode').command('messages_copy')
+      end,
+    },
+    {
+      '<leader>o+',
+      function()
+        require('opencode').append_prompt('@buffer')
+      end,
+    },
+    {
+      '<leader>o+',
+      mode = { 'x' },
+      function()
+        require('opencode').append_prompt('@selection')
+      end,
+    },
+  },
+  config = function()
+    vim.g.opencode_opts = {
+      terminal = {
+        win = {
+          enter = true,
+        },
+      },
+    }
+
+    -- vim.keymap.set({ 'n', 'v' }, '<leader>os', function()
+    --   require('opencode').select()
+    -- end, { desc = 'Select opencode prompt' })
+
+    -- Example: keymap for custom prompt
+    -- vim.keymap.set('n', '<leader>oe', function()
+    --   require('opencode').prompt('Explain @cursor and its context')
+    -- end, { desc = 'Explain this code' })
+  end,
+}
