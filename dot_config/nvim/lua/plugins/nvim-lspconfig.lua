c565992916d1e13918d69b84c60ab195421d7956
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-10-06 19:02:03 -0700

    feat(nvim-lspconfig): configure vtsls and tsserver LSP settings.

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -74,16 +74,24 @@ return {
         settings = {
           vtsls = {
             autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+                entriesLimit = 10,
+              },
+            },
           },
           typescript = {
             tsserver = {
               experimental = {
                 -- NOTE: Some projects will crawl to a halt when this is enabled
-                enableProjectDiagnostics = true,
+                -- enableProjectDiagnostics = true,
               },
+              maxTsServerMemory = 8192,
             },
             preferences = {
               importModuleSpecifier = 'non-relative',
+              autoImportFileExcludePatterns = { '.git/*' },
             },
             updateImportsOnFileMove = 'always',
           },
