668f7d72c96fad424052872461d2593d9baa8659
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-10-09 01:17:32 -0700

    feat(cmd.lua): add autocmd to delete terminal buffers on exit.

--- dot_config/nvim/lua/config/cmd.lua
+++ dot_config/nvim/lua/config/cmd.lua
@@ -49,3 +49,14 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.bo.buflisted = false
   end,
 })
+
+vim.api.nvim_create_autocmd('ExitPre', {
+  pattern = '*',
+  callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+  end,
+})
