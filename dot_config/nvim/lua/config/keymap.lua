6a2167c4b9c6e4716904c9c0b52e8543d21d52c4
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-10-13 14:29:04 -0700

    feat(keymap.lua): add window navigation keymaps with Ctrl + hjkl.

--- dot_config/nvim/lua/config/keymap.lua
+++ dot_config/nvim/lua/config/keymap.lua
@@ -1,3 +1,8 @@
+vim.keymap.set('n', '<C-j>', '<C-W>j')
+vim.keymap.set('n', '<C-k>', '<C-W>k')
+vim.keymap.set('n', '<C-h>', '<C-W>h')
+vim.keymap.set('n', '<C-l>', '<C-W>l')
+
 vim.keymap.set('n', 'vv', 'V')
 
 vim.keymap.set('n', '<C-W>|', '<C-W>v')
