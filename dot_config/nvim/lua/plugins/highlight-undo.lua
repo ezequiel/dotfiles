6f036db129f94cdd319bb142c1cd642afa89e595
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-11-01 04:20:20 -0700

    feat: add highlight-undo plugin to highlight text after undo operations.

---
+++ dot_config/nvim/lua/plugins/highlight-undo.lua
@@ -0,0 +1,10 @@
+return {
+  'tzachar/highlight-undo.nvim',
+  opts = {
+    hlgroup = 'HighlightUndo',
+    duration = 750,
+    pattern = { '*' },
+    ignored_filetypes = {},
+    ignore_cb = nil,
+  },
+}
