2ef88ca0a1de25ef7051edf3ff048699bb4f82e6
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2025-11-01 02:43:41 -0700

    feat: remove large_file plugin as it is no longer needed

--- dot_config/nvim/lua/plugins/large_file.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'mireq/large_file',
-  opts = {
-    size_limit = 512 * 1024, -- 512kiB
-    on_large_file_read_pre = function(_)
-      vim.notify('Opening a large file!', vim.log.levels.WARN)
-    end,
-  },
-}
