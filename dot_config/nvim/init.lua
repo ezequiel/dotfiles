8d4f5d17a61b78def703042c358fcd52b691ca99
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-02-27 10:06:34 -0800

    nvim: remove comment plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -475,17 +475,6 @@ end)
 ----------------------------------------------------
 
 vim.pack.add({
-  'https://github.com/JoosepAlviste/nvim-ts-context-commentstring',
-  'https://github.com/numToStr/Comment.nvim',
-})
-require('Comment').setup({
-  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-})
-
-----------------------------------------------------
-----------------------------------------------------
-
-vim.pack.add({
   'https://github.com/okuuva/auto-save.nvim',
 })
 
