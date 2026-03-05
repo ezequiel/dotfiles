5e541ec054663029f6c8ac0c905101531a889349
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-03-05 08:55:26 -0800

    nvim: fix grug-far setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -707,67 +707,66 @@ vim.pack.add({
 
 require('grug-far').setup({
   folding = { enabled = false },
+  helpLine = { enabled = false },
+  icons = { enabled = false },
   resultLocation = { showNumberLabel = false },
+  showInputsTopPadding = false,
   wrap = false,
   keymaps = {
-    abort = false,
-    applyNext = false,
-    applyPrev = false,
-    close = { n = '<c-c>' },
-    gotoLocation = { n = '<enter>' },
-    help = { n = '?' },
-    historyAdd = false,
-    historyOpen = false,
-    nextInput = { n = '<tab>' },
-    openLocation = false,
-    openNextLocation = false,
-    openPrevLocation = false,
-    pickHistoryEntry = false,
-    prevInput = { n = '<s-tab>' },
-    previewLocation = false,
+    close = false,
     qflist = { n = '<C-q>' },
     refresh = { n = 'R' },
-    replace = { n = '<localleader>rr' },
-    swapEngine = { n = '<localleader>ee' },
-    swapReplacementInterpreter = false,
-    syncFile = { n = '<localleader>sf' },
-    syncLine = { n = '<localleader>sl' },
-    syncLocations = { n = '<localleader>sa' },
-    syncNext = false,
-    syncPrev = false,
-    toggleShowCommand = false,
   },
 })
 
-vim.keymap.set('n', '<leader>gf', require('grug-far').open)
+local function grug_far_open(opts)
+  local grug_far = require('grug-far')
+  if grug_far.has_instance('1337') then
+    grug_far.kill_instance('1337')
+  end
+  grug_far.open(vim.tbl_extend('force', { instanceName = '1337' }, opts or {}))
+end
+
+vim.keymap.set('n', '<leader>gb', function()
+  grug_far_open({ prefills = { paths = vim.fn.expand('%') } })
+end)
+
+vim.keymap.set('n', '<leader>gf', function()
+  grug_far_open()
+end)
+
+vim.keymap.set('n', '<leader>gr', function()
+  require('grug-far').toggle_instance({ instanceName = '1337' })
+end)
+
+vim.keymap.set('n', '<leader>gw', function()
+  grug_far_open({
+    prefills = {
+      flags = '--case-sensitive --fixed-strings --word-regexp',
+      search = vim.fn.expand('<cword>'),
+    },
+  })
+end)
+
+vim.keymap.set('x', '<leader>gv', function()
+  grug_far_open({
+    prefills = {
+      search = vim.fn.getreg('/'),
+      flags = '--case-sensitive --fixed-strings',
+    },
+  })
+end)
 
 vim.api.nvim_create_autocmd('FileType', {
   group = augroup,
   pattern = 'grug-far',
   callback = function()
     vim.opt_local.signcolumn = 'no'
-    vim.keymap.set('i', '<CR>', '<cmd>stopinsert<CR>', { buffer = true })
+    vim.keymap.set('n', '<c-c>', function()
+      require('grug-far').hide_instance('1337')
+    end, { buffer = true })
   end,
 })
-vim.keymap.set('n', '<leader>gb', function()
-  require('grug-far').open({ prefills = { paths = vim.fn.expand('%') } })
-end)
-vim.keymap.set('n', '<leader>gw', function()
-  require('grug-far').open({
-    prefills = {
-      flags = '--case-sensitive',
-      search = '\\b' .. vim.fn.expand('<cword>') .. '\\b',
-    },
-  })
-end)
-vim.keymap.set('x', '<leader>gv', function()
-  require('grug-far').open({
-    prefills = {
-      search = vim.fn.getreg('/'),
-      flags = '--case-sensitive',
-    },
-  })
-end)
 
 ----------------------------------------------------
 ----------------------------------------------------
@@ -909,7 +908,7 @@ local lsp_opts = {
         experimental = {
           completion = {
             enableServerSideFuzzyMatch = true,
-            entriesLimit = 1337,
+            entriesLimit = '1337',
           },
         },
       },
