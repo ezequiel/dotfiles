6862a91be13e7900320957b9e7d0110cf7125cfb
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-03-11 14:37:10 -0700

    nvim: fix ufo config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -624,7 +624,9 @@ vim.pack.add({
   'https://github.com/kevinhwang91/nvim-ufo',
 })
 
+vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
 
 require('ufo').setup({
   provider_selector = function()
