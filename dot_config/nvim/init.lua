bd36e1cc800c6aa2a4ce164698095543d1d93944
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-03-11 14:35:25 -0700

    nvim: fix augroup creation

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -37,7 +37,7 @@ vim.diagnostic.config({
 ----------------------------------------------------
 ----------------------------------------------------
 
-local augroup = vim.api.nvim_create_augroup('my-config', { clear = false })
+local augroup = vim.api.nvim_create_augroup('my-config', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
   group = augroup,
