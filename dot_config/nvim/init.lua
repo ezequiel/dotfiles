f1ecfbf1175c7c1bbd43bf8fe9d268f70c2fc2e0
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-03-10 23:52:09 -0700

    nvim: remove unnecessary vim.cmd call

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1053,15 +1053,13 @@ vim.keymap.set('n', 'gH', require('gitportal').open_file_in_neovim)
 ----------------------------------------------------
 ----------------------------------------------------
 
-vim.cmd([[
-  let g:vindent_block_ending = [ ')', ']', '}', 'end', 'else', 'elif' ]
-  let g:vindent_infer	= 1
-  let g:vindent_jumps = 1
-  let g:vindent_motion_XX_se = ']s'
-  let g:vindent_motion_XX_ss = '[s'
-  let g:vindent_object_XX_aI = 'ai'
-  let g:vindent_object_XX_ii = 'ii'
-]])
+vim.g.vindent_block_ending = { ')', ']', '}', 'end', 'else', 'elif' }
+vim.g.vindent_infer = 1
+vim.g.vindent_jumps = 1
+vim.g.vindent_motion_XX_se = ']s'
+vim.g.vindent_motion_XX_ss = '[s'
+vim.g.vindent_object_XX_aI = 'ai'
+vim.g.vindent_object_XX_ii = 'ii'
 
 vim.pack.add({ 'https://github.com/jessekelighine/vindent.vim' })
 
