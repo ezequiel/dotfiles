a732f16ad2cec8c8fc2f9f2c5d8ec5cd8d82d44a
Ezequiel Rodriguez <ezerodriguez@tesla.com>
2026-03-14 19:49:53 -0700

    nvim: fix copilot lazy loading

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -549,12 +549,14 @@ require('nvim-surround').setup()
 ----------------------------------------------------
 ----------------------------------------------------
 
+vim.pack.add({
+  'https://github.com/zbirenbaum/copilot.lua',
+}, { load = function() end })
+
 vim.api.nvim_create_autocmd('InsertEnter', {
   once = true,
   callback = function()
-    vim.pack.add({
-      'https://github.com/zbirenbaum/copilot.lua',
-    })
+    vim.cmd.packadd('copilot.lua')
 
     require('copilot').setup({
       server_opts_overrides = {
